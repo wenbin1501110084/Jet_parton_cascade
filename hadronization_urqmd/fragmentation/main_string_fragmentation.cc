89a2c4d43d7b5a146026be625e108488bab9628e
Wenbin <1501110084@pku.edu.cn>
2023-10-18 14:20:10 -0700

    Add CP5 parameters in fragmentation

--- hadronization_urqmd/fragmentation/main_string_fragmentation.cc
+++ hadronization_urqmd/fragmentation/main_string_fragmentation.cc
@@ -67,6 +67,27 @@ int main(int argv, char* argc[])
     pythia.readString("ColourReconnection:timeDilationMode = 3");    
     pythia.readString("ColourReconnection:timeDilationPar = 0.18");  
     pythia.readString("ProcessLevel:all = off"); // The trick!
+    // CMS CP5 setting
+    pythia.readString("Tune:pp=14");
+    pythia.readString("Tune:ee=7");
+    pythia.readString("MultipartonInteractions:ecmPow=0.03344");
+    pythia.readString("MultipartonInteractions:bProfile=2");
+    pythia.readString("MultipartonInteractions:pT0Ref=1.407");
+    pythia.readString("MultipartonInteractions:coreRadius=0.6671");
+    pythia.readString("MultipartonInteractions:coreFraction=0.4281");
+    pythia.readString("ColourReconnection:range=4.881");
+    pythia.readString("SigmaTotal:zeroAXB=off");
+    pythia.readString("SpaceShower:alphaSorder=2");
+    pythia.readString("SpaceShower:alphaSvalue=0.118");
+    pythia.readString("SigmaProcess:alphaSvalue=0.118");
+    pythia.readString("SigmaProcess:alphaSorder=2");
+    pythia.readString("MultipartonInteractions:alphaSvalue=0.118");
+    pythia.readString("MultipartonInteractions:alphaSorder=2");
+    pythia.readString("TimeShower:alphaSorder=2");
+    pythia.readString("TimeShower:alphaSvalue=0.118");
+    pythia.readString("SigmaTotal:mode = 0");
+    pythia.readString("SigmaTotal:sigmaEl = 21.89");
+    pythia.readString("SigmaTotal:sigmaTot = 100.309");
     pythia.readString(" StringFlav:probStoUD=0.50");
     //pythia.readString("StringFlav:BtoMratio=0.5");
     pythia.readString("StringFlav:probQQtoQ=0.34");
